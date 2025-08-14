f8f90316a074dff23f54de2831469c03f6d461a7
Veena Ghorakavi <vghorakavi@gmail.com>
2025-08-14 10:38:03 -0500

    Added an argparser that handles running tasks and not planning tasks.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+import argparse
 
 @dataclass(frozen=True)
 class Task:
@@ -8,6 +9,14 @@ class Task:
 
 def main():
     return
+
+def build_arg_parser():
+    #hHandles running tasks at the moment. 
+    argparser = argparse.ArgumentParser(description="Scheduler")
+    argparser.add_argument("-f", "--file", default="-", help="Task list or '-' for stdin")
+    argparser.add_argument("--format", choices=["text", "json"], default="text")
+    argparser.add_argument("--run", action="store_true", help="Run tasks")
+    return argparser
 
 if __name__ == "__main__":
     main()
